5aa42bde1bfc1187af5b1702bdb7e8340bd2529a
gostlore <52397115+gostlore@users.noreply.github.com>
2020-04-18 18:47:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var expression = function(a, b, c = "Declaration1") {
+    if (arguments.length == 0) {
+        return console.log("Error")
+    }
+	return console.log(a += 1), console.log(b += 2), console.log(c += '_ok')
+}
+console.log(expression(1, 2));
+
+
+
+
+
+function declaration(a, b, c) {
+    if (arguments.length == 0) {
+
+		return console.log(a += 1), console.log(b += 2), console.log(c += '_ok')
+
+    }
+    return("false")
+}
+console.log(declaration(1, 2));
